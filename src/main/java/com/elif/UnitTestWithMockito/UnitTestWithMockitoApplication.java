b2f521c6ece2deace04bd59b4e86ed40c5a41734
elifseven <svnelif34@gmail.com>
2019-05-01 22:33:51 +0300

    first-commit

---
+++ src/main/java/com/elif/UnitTestWithMockito/UnitTestWithMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.elif.UnitTestWithMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestWithMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestWithMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elif/UnitTestWithMockito/UnitTestWithMockitoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.elif.UnitTestWithMockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnitTestWithMockitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
